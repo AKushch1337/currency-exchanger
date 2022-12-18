cecf2385672fac802f0eabc9a04e6cd9b1faffc7
Artem Kushch <artemkusch2@gmail.com>
2022-12-18 23:20:06 +0200

    Finished implementing UI.

--- app/src/main/java/com/example/currencyexchangeapp/composables/ConverterScreen.kt
+++ app/src/main/java/com/example/currencyexchangeapp/composables/ConverterScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BottomSheetScaffold
@@ -15,14 +17,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.currencyexchangeapp.ui.theme.Purple500
 import com.example.currencyexchangeapp.utils.Constants.Companion.BAR_TITLE
+import com.example.currencyexchangeapp.utils.Constants.Companion.CURRENCY_CODES_LIST
 import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
@@ -54,7 +59,29 @@ fun ConverterScreen() {
     var isFromSelected = true
 
     BottomSheetScaffold(
-        sheetContent = {},
+        sheetContent = {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(275.dp)) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    items(CURRENCY_CODES_LIST) { item ->
+                        Text(text = "${item.currencyCode}\t ${item.countryName}",
+                            modifier = Modifier
+                                .padding(vertical = 5.dp)
+                                .clickable {
+                                    if (isFromSelected) {
+                                        fromCurrencyCode.value = item.currencyCode
+                                    } else {
+                                        toCurrencyCode.value = item.currencyCode
+                                    }
+                                    scope.launch { scaffoldState.bottomSheetState.collapse() }
+                                })
+                    }
+                }
+            }
+        },
         topBar = {
             Spacer(modifier = Modifier.padding(15.dp))
             TopAppBar(
@@ -62,9 +89,10 @@ fun ConverterScreen() {
                     Text(
                         text = BAR_TITLE,
                         modifier = Modifier.fillMaxWidth(),
-                        fontSize = 24.sp,
+                        fontSize = 35.sp,
                         textAlign = TextAlign.Center,
-                        color = if (isDarkMode) Color.White else Color.Black)
+                        color = if (isDarkMode) Color.White else Color.Black,
+                        style = TextStyle(fontWeight = FontWeight.Black))
                 },
                 backgroundColor = if (isDarkMode) Color.Black else Color.White, elevation = 0.dp)
         },
@@ -82,6 +110,8 @@ fun ConverterScreen() {
                 .fillMaxWidth()
                 .height(50.dp)
                 .clickable {
+                    isFromSelected = true
+                    scope.launch { scaffoldState.bottomSheetState.expand() }
                 }
                 .border(1.dp, Color.Black, RoundedCornerShape(5.dp)),
                 contentAlignment = Alignment.CenterStart) {
@@ -95,6 +125,8 @@ fun ConverterScreen() {
                 .fillMaxWidth()
                 .height(50.dp)
                 .clickable {
+                    isFromSelected = false
+                    scope.launch { scaffoldState.bottomSheetState.expand() }
                 }
                 .border(1.dp, Color.Black, RoundedCornerShape(5.dp)),
                 contentAlignment = Alignment.CenterStart) {
@@ -137,6 +169,22 @@ fun ConverterScreen() {
                 Text(text = "CONVERT", fontSize = 25.sp)
 
             }
+            Spacer(modifier = Modifier.padding(15.dp))
+            Text(
+                text = convertedAmount.value,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                fontSize = 30.sp,
+                color = Color.Black,
+                style = TextStyle(textAlign = TextAlign.Center))
+            Spacer(modifier = Modifier.padding(10.dp))
+            Text(
+                text = singleConvertedValue.value,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                fontSize = 30.sp,
+                color = Color.Black,
+                style = TextStyle(textAlign = TextAlign.Center))
         }
     }
 }
