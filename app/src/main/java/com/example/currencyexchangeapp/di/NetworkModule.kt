3fe6ee377aa622bdfd9f911c16844d6bd7d00f7b
Artem Kushch <artemkusch2@gmail.com>
2022-12-25 23:51:17 +0200

    Created object NetworkModule

---
+++ app/src/main/java/com/example/currencyexchangeapp/di/NetworkModule.kt
@@ -0,0 +1,53 @@
+package com.example.currencyexchangeapp.di
+
+import com.example.currencyexchangeapp.data.CurrencyRatesAPI
+import com.example.currencyexchangeapp.utils.Constants.Companion.BASE_URL
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.sql.Time
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(gsonConverterFactory)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideCurrencyRatesAPI(retrofit: Retrofit): CurrencyRatesAPI {
+        return retrofit.create(CurrencyRatesAPI::class.java)
+    }
+}
